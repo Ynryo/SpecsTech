6401f5ce54b1272a9114554b762041857f0f10ac
Ynryo <ewenatideklein@gmail.com>
2023-07-17 19:02:51 +0200

    automatization assets

--- articles/nvidia/rtx/40/90/index.php
+++ articles/nvidia/rtx/40/90/index.php
@@ -12,18 +12,8 @@
     <meta property="og:url" content="https://specstech.fr/nvidia/rtx/40/90/" />
     <meta property="og:description" content="Retrouvez toutes les informations techniques de vos cartes graphiques">
     <link rel="canonical" href="https://specstech.fr/nvidia/rtx/40/90/">
-    <link rel="icon" type="image/png" href="/assets/favicon/favicon-50x50.png">
-    <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/link.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/menu.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/header.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/footer.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/specs-table.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
-    <noscript>Votre navigateur bloque le JavaScript. Merci de l'activer pour pouvoir utiliser le site à son plein
-        potentiel.</noscript>
+    <?php include(dirname(__FILE__, 1) . '/assets/src/assets.php') ?>
 </head>
 
 <body>
