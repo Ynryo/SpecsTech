830cb6801b8789d565582d75baf449dafb2484ff
Ynryo <ewenatideklein@gmail.com>
2023-09-06 17:39:07 +0200

    automatize specs getting

--- assets/src/header.php
+++ assets/src/header.php
@@ -1,18 +1,10 @@
 <header>
     <a class="icon nolink" href="/">
-<<<<<<< HEAD
         <h1>SpecsTech</h1>
         <!-- <img src="/assets/icons-assets/gradient-shadow.png"> -->
     </a>
     <form method="get" action="/search/">
         <input type="search" class="search-bar" placeholder="Recherchez des cartes graphiques" id="search-bar" name="q" role="combobox" aria-roledescription="combobox">
-=======
-        <h1>Specs-Tech</h1>
-        <!-- <img src="/assets/icons-assets/gradient-shadow.png"> -->
-    </a>
-    <form novalidate method="post" action="/search/?" aria-disabled="true" disabled>
-        <input type="search" class="search-bar" placeholder="Recherchez des cartes graphiques" id="search-bar" name="search" role="combobox" aria-roledescription="combobox" disabled>
->>>>>>> 7412203ff051df9eae8c6e4944dc723ca260db15
     </form>
     <nav>
         <ul>
@@ -28,13 +20,8 @@
         <div class="bar3"></div>
     </div>
     <div class="menu" id="menu">
-<<<<<<< HEAD
         <form method="get" action="/search/">
             <input type="text" class="search-bar" placeholder="Recherchez des cartes graphiques" id="search-bar" name="q" role="combobox" aria-roledescription="combobox">
-=======
-        <form>
-            <input type="text" class="search-bar" placeholder="Recherchez des cartes graphiques" id="search-bar" name="search" role="combobox" aria-roledescription="combobox" disabled>
->>>>>>> 7412203ff051df9eae8c6e4944dc723ca260db15
         </form>
         <nav>
             <ul>
