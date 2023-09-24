7dd60c6907b26d01ca71ceb8d7839e8f628af03a
Ynryo <ewenatideklein@gmail.com>
2023-09-24 21:48:40 +0200

    add maintenance mode

--- maintenance.php
+++ maintenance.php
@@ -2,38 +2,25 @@
 <html lang="fr-FR">
 
 <head>
-    <title>Specs-Tech</title>
+    <title>SpecsTech - Maintenance en cours</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="SpecsTech" />
-    <meta name="description" content="Retrouvez toutes les informations techniques de vos cartes graphiques">
+    <meta name="description" content="Maintenance du site en cours">
     <meta property="og:title" content="Accueil ǀ SpecsTech" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://specstech.fr" />
-    <meta property="og:description" content="Retrouvez toutes les informations techniques de vos cartes graphiques">
-    <link rel="canonical" href="https://specstech.fr">
+    <meta property="og:description" content="Maintenance du site en cours">
     <link rel="stylesheet" type="text/css" href="/assets/css/maintenance.css">
-    <?php include(dirname(__FILE__, 1) . '/assets/src/assets.php') ?>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
-    <header>
-    <div class="icon">
-        <h1>Specs-Tech</h1>
-        <!-- <img src="/assets/icons-assets/gradient-shadow.png"> -->
-</div>
-</header>
-
-    <section class="hero">
-        <div class="hero-content">
-            <h2>&#128679; Maintenance <span class="colored-logo">du site</span> &#128679;</h2>
-            <img src="/assets/svg/header-line.svg" alt="" srcset="/assets/svg/header-line.svg">
-            <p>Specs-Tech est actuellement en maintenance. Nous faisons tout notre possible pour réouvrir au plus vite.<br>Nous vous remercions pour votre patience.</p>
-        </div>
+    <section class="main">
+        <h2><span class="colored-logo">Maintenance</span><br>du site</h2>
+        <p>SpecsTech est actuellement en maintenance. Nous faisons tout notre possible pour réouvrir au plus vite.<br>Nous vous remercions pour votre patience.</p>
     </section>
-
-    <?php include(dirname(__FILE__, 1) . '/assets/src/footer.php') ?>
-    <script type="text/javascript" src="/assets/js/modal.js"></script>
 </body>
 
 </html>
