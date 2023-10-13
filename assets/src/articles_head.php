beba0124f5849c71d100a843285e1d69e8fabb36
Ynryo <ewenatideklein@gmail.com>
2023-10-13 18:16:20 +0200

    update

--- assets/src/articles_head.php
+++ assets/src/articles_head.php
@@ -1,3 +1,7 @@
+<?php
+$card_id_split = explode("/", "/" . str_replace("_", "/", $id) . "/");
+$path = "/" . $card_id_split[1] . "/" . $card_id_split[2] . "/" . substr($card_id_split[3], 0, 2) . "/" . substr($card_id_split[3], 2) . "/";
+?>
 <title>SpecsTech - <?php echo $row['card_name'] ?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -5,7 +9,7 @@
 <meta name="description" content="Retrouvez toutes les informations techniques de vos cartes graphiques">
 <meta property="og:title" content="SpecsTech - <?php echo $row['card_name'] ?>" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://<?php echo $_SERVER['SERVER_NAME'] . "/" . basename(dirname(__FILE__, 4)) . "/" . basename(dirname(__FILE__, 3)) . "/" . basename(dirname(__FILE__, 2)) . "/" . basename(dirname(__FILE__, 1)) . "/" ?>" />
+<meta property="og:url" content="https://<?php echo $_SERVER['SERVER_NAME'] . "/articles" . $path?>" />
 <meta property="og:description" content="Retrouvez toutes les informations techniques de vos cartes graphiques">
-<link rel="canonical" href="https://<?php echo $_SERVER['SERVER_NAME'] . "/" . basename(dirname(__FILE__, 4)) . "/" . basename(dirname(__FILE__, 3)) . "/" . basename(dirname(__FILE__, 2)) . "/" . basename(dirname(__FILE__, 1)) . "/" ?>">
+<link rel="canonical" href="https://<?php echo $_SERVER['SERVER_NAME'] . "/articles" . $path ?>">
 <link rel="stylesheet" type="text/css" href="/assets/css/specs-table.css">
