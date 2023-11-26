a5e447285c983a39679537970ed03d9bc556055f
Ynryo <ewenatideklein@gmail.com>
2023-11-27 00:47:26 +0100

    theme color update

--- assets/src/assets.php
+++ assets/src/assets.php
@@ -1,5 +1,5 @@
 <meta property="og:site_name" content="SpecsTech">
-<meta name="theme-color"content="#1900ff">
+<meta name="theme-color"content="#111">
 <link rel="manifest" href="/app.webmanifest">
 <link rel="icon" type="image/png" href="/assets/favicon/favicon-50x50.png">
 <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
