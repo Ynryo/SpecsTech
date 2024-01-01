c5da5a757bf7e266ddb0964efb75fd861aa3f928
Ynryo <ewenatideklein@gmail.com>
2024-01-01 15:46:00 +0100

    status code when maintenance mode is on change

--- assets/src/files_top.php
+++ assets/src/files_top.php
@@ -7,6 +7,7 @@ if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if (isset($row["action"]) && $row["action"] == "maintenance_mode_on") {
         // echo $row["action"];
+        http_response_code(503);
         header('Location: /maintenance/');
         exit(); // Assurez-vous de terminer le script après une redirection header
     }
