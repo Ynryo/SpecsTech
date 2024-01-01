6a9576e97a3fe792ac98ad12c6de876453fb2fdc
Ynryo <ewenatideklein@gmail.com>
2024-01-01 15:49:32 +0100

    font update

--- assets/src/assets.php
+++ assets/src/assets.php
@@ -7,6 +7,5 @@
 <link rel="stylesheet" type="text/css" href="/assets/css/menu.css">
 <link rel="stylesheet" type="text/css" href="/assets/css/header.css">
 <link rel="stylesheet" type="text/css" href="/assets/css/footer.css">
-<link rel="stylesheet" type="text/font" href="/assets/fonts/Montserrat-VariableFont_wght.ttf">
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3505338734539298" crossorigin="anonymous"></script>
 <noscript>Votre navigateur bloque le JavaScript. Merci de l'activer pour pouvoir utiliser le site à son plein potentiel.</noscript>
