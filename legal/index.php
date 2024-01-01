bfa96b0b01d859832d9a4666439fb8b4ef8ae80b
Ynryo <ewenatideklein@gmail.com>
2024-01-01 19:28:23 +0100

    email was changed

--- legal/index.php
+++ legal/index.php
@@ -29,11 +29,11 @@
 
     <section class="main legal">
         <h2>1 - Édition du site.</h2>
-        <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://specstech.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
-            <br><br><strong>Propriétaire du site :</strong> Aaron LOPEZ - Contact : aaron@specstech.fr.
-            <br><br><strong>Directeur de la publication :</strong> Aaron LOPEZ - Contact : aaron@specstech.fr.
+        <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://specstech.fr/ l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
+            <br><br><strong>Propriétaire du site :</strong> Aaron LOPEZ - Contact : aaron.lopez@specstech.fr.
+            <br><br><strong>Directeur de la publication :</strong> Aaron LOPEZ - Contact : aaron.lopez@specstech.fr.
             <br><br><strong>Hébergeur :</strong> PlanetHoster 4416 Louis-B.-Mayer Laval, Québec Canada H7P 0G1 - Téléphone : +1 855-774-4678
-            <br><br><strong>Délégué à la protection des données :</strong> Aaron LOPEZ - aaron@specstech.fr
+            <br><br><strong>Délégué à la protection des données :</strong> Aaron LOPEZ - aaron.lopez@specstech.fr
         </p>
         <h2>2 - Propriété intellectuelle et contrefaçons.</h2>
         <p><strong>Aaron LOPEZ</strong> est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.
@@ -41,17 +41,17 @@
             <br><br>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
         </p>
         <h2>3 - Limitations de responsabilité.</h2>
-        <p><strong>Aaron LOPEZ</strong> ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://specstech.fr.
-            <br><br><strong>Aaron LOPEZ</strong> décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://specstech.fr.
-            <br><br><strong>Aaron LOPEZ</strong> s’engage à sécuriser au mieux le site https://specstech.fr, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.
+        <p><strong>Aaron LOPEZ</strong> ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://specstech.fr/.
+            <br><br><strong>Aaron LOPEZ</strong> décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://specstech.fr/.
+            <br><br><strong>Aaron LOPEZ</strong> s’engage à sécuriser au mieux le site https://specstech.fr/, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.
             <br><br>Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. <strong>Aaron LOPEZ</strong> se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.
             <br><br>Le cas échéant, <strong>Aaron LOPEZ</strong> se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).
         </p>
         <h2>4 - CNIL et gestion des données personnelles.</h2>
-        <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://specstech.fr dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Aaron LOPEZ - aaron@specstech.fr.</p>
+        <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://specstech.fr/ dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Aaron LOPEZ - aaron.lopez@specstech.fr.</p>
         <h2>5 - Liens hypertextes et cookies.</h2>
-        <p>Le site https://specstech.fr contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://specstech.fr.
-            <br><br>La navigation sur le site https://specstech.fr est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
+        <p>Le site https://specstech.fr/ contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers https://specstech.fr/.
+            <br><br>La navigation sur le site https://specstech.fr/ est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
             <br><br>Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.
             <br><br>Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.
             <br><br>Les cookies sont enregistrés pour une durée maximale de 12 mois.
